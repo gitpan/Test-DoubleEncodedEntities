fdc6595ef0b79fccf36334b5a5aab554b43bf4a5
Mark Fowler <mark@twoshortplanks.com>
2012-01-13 19:55:12 +0000

    Import of MARKF/Test-DoubleEncodedEntities-2.00 from CPAN.
    gitpan-cpan-distribution: Test-DoubleEncodedEntities
    gitpan-cpan-version:      2.00
    gitpan-cpan-path:         MARKF/Test-DoubleEncodedEntities-2.00.tar.gz
    gitpan-cpan-author:       MARKF
    gitpan-cpan-maturity:     released

--- lib/Test/DoubleEncodedEntities.pm
+++ lib/Test/DoubleEncodedEntities.pm
@@ -1,57 +1,58 @@
 package Test::DoubleEncodedEntities;
+use base qw(Exporter);
 
+use 5.006;
 use strict;
 use warnings;
 
+our @EXPORT;
+our $VERSION = "2.00";
+
 use HTML::TokeParser::Simple;
 use Test::DoubleEncodedEntities::Entities;
+
+use Carp qw(croak);
+
 use Test::Builder;
-require Exporter;
-
-use vars qw(@EXPORT @ISA $VERSION);
-@EXPORT = qw(ok_dee);
-@ISA = qw(Exporter);
-
-$VERSION = "0.01";
-
-my $Tester = Test::Builder->new();
+my $tester = Test::Builder->new();
 
 my $entities = join "|", @entities;
 
-sub ok_dee
-{
+sub ok_dee {
   my $input = shift;
   my $name  = shift || "double encoded entity test";
 
   # parse the input
-  my $p = HTML::TokeParser::Simple->new( doc => $input )
-   or die "Can't parse input";
+  my $p = HTML::TokeParser::Simple->new( \$input )
+   or croak "Can't parse input";
 
   $p->unbroken_text(1);
 
   # search all text bits for problems
   my %oops;
-  while ( my $token = $p->get_token )
-  {
+  while ( my $token = $p->get_token ) {
     next unless $token->is_text;
     my $string = $token->as_is;
 
     # look for bad entities
-    $oops{$_}++ foreach $string =~ m/&amp;($entities|#\d+);/go;
+    $oops{$_}++ foreach $string =~ m/(&(?:amp|\#0*38);(?:$entities|\#\d+);)/gox;
   }
 
   # did we get away okay?
-  unless(%oops)
-    { return $Tester->ok(1,$name) }
+  unless(%oops) {
+    return $tester->ok(1,$name)
+  }
 
   # report the problem
-  $Tester->ok(0, $name);
-  foreach (sort { $a cmp $b } keys %oops)
-   { $Tester->diag(qq{Found $oops{$_} "&amp;$_;"\n}) }
+  $tester->ok(0, $name);
+  foreach (sort { $a cmp $b } keys %oops) {
+    $tester->diag(qq{Found $oops{$_} "$_"\n})
+  }
 
   # return 0 as we got an error
   return 0;
 }
+push @EXPORT, "ok_dee";
 
 =head1 NAME
 
@@ -66,26 +67,45 @@ Test::DoubleEncodedEntities - check for double encoded entities
 
 =head1 DESCRIPTION
 
-This is a testing module that checks for double encoded entities
-in your string.  It exports one function ok_dee that does
-the testing.  You should pass it the html you want to test
-and optionally the name for the test.  It will run the test
-producing B<Test::Harness> compatible output, and will return
-1 on a pass and 0 on a failure.
+This testing module huristically checks for double
+encoded HTML entities in your string.
 
-Note that this only checks the body text;  Entities in attributes
+=head2 Functions
+
+This module automatically exports the following function:
+
+=over
+
+=item ok_dee($string)
+
+=item ok_dee($string, $test_description)
+
+This module knows about all the entities defined in the HTML5
+working draft and numerical entities.
+
+=back
+
+=head1 BUGS
+
+This module only checks the body text;  Entities in attributes
 are ignored as often you may want to double encoded entities on
 purpose in things like URLs.
 
-This module knows about all the entities defined in the HTML 4.0
-specification, and numerical entities.
+Bugs (and requests for new features) can be reported though the CPAN
+RT system:
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-DoubleEncodedEntities>
+
+Alternatively, you can simply fork this project on github and
+send me pull requests.  Please see L<http://github.com/2shortplanks/Test-DoubleEncodedEntities>
 
 =head1 AUTHOR
 
-  Copyright Mark Fowler 2004.  All rights reserved.
+Written by Mark Fowler B<mark@twoshortplanks.com>
 
-  This program is free software; you can redistribute it
-  and/or modify it under the same terms as Perl itself.
+Copyright Mark Fowler 2004, 2011, 2012.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
